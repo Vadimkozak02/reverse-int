bb5c37623943395f67a085623648d17442977286
Vadim <vadimkozak02@gmail.com>
2021-03-15 19:54:46 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n < 0) {
+        n *= -1;
+        return n.toFixed(0).split('').reverse().join('')-0;
+    } else {
+    return n.toFixed(0).split('').reverse().join('')-0;
+    }
+};
